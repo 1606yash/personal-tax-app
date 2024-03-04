9d905969ec14a7c16391b35738c6a9587aefbe83
Yash Goyal <127027150+1606yash@users.noreply.github.com>
2024-03-04 16:21:48 +0530

    upload missing files

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'picco/yash',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '8f0e31c8937cc22a0b9737a448451b1b9ea57505',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'firebase/php-jwt' => array(
+            'pretty_version' => 'v6.10.0',
+            'version' => '6.10.0.0',
+            'reference' => 'a49db6f0a5033aef5143295342f1c95521b075ff',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../firebase/php-jwt',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'picco/yash' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '8f0e31c8937cc22a0b9737a448451b1b9ea57505',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
